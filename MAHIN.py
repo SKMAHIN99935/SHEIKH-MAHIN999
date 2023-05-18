93e2b26589d426ae215816010f5147bae1b4d731
SK MH99935 <104576528+SKMAHIN99935@users.noreply.github.com>
2023-05-18 20:57:25 +0600

    Update MAHIN.py

--- MAHIN.py
+++ MAHIN.py
@@ -87,7 +87,7 @@ logo =                                          """
 \033[1;37m------------------------------------------------
 \033[1;37m Owner   :            SHEIKH MAHIN
 \033[1;37m Facebook:            SK MH
-\033[1;37m Github  :            SK-MAHIN99935
+\033[1;37m Github  :            https://github.com/SKMAHIN99935
 \033[1;37m Version :            0.1
 \033[1;37m------------------------------------------------ """
 def clear():
